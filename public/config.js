ed78f745a2460ef16ea3937492e90527f748a4b9
Mario Reder <mario.reder@pm.me>
2026-05-28 03:58:12 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -27,7 +27,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "My Test DEX",
   "VITE_SEO_SITE_DESCRIPTION": "{{constructor.constructor(\"alert(1)\")()}}",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/orderly-dex-8824/",
+  "VITE_SEO_SITE_URL": "https://stjohnbugbounty.xyz",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
@@ -37,7 +37,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_USE_CUSTOM_PNL_POSTERS": "false",
   "VITE_CUSTOM_PNL_POSTER_COUNT": "0",
   "VITE_TRADING_VIEW_COLOR_CONFIG": "",
-  "VITE_ANALYTICS_SCRIPT": "",
+  "VITE_ANALYTICS_SCRIPT": "\u001b��]u�]u�",
   "VITE_SYMBOL_LIST": "",
   "VITE_RESTRICTED_REGIONS": "",
   "VITE_WHITELISTED_IPS": ""
